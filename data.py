92c90a3b8ba47e7f01cef2c060ad490c79682df2
gu1m <106485054+gu1m@users.noreply.github.com>
2023-05-20 16:53:48 -0300

    Update data.py

--- data.py
+++ data.py
@@ -68,7 +68,7 @@ if indi_geral == "Individual":
         
         # se o usuario escolher para analisar artista
         elif selecionado == "Artista":
-            if keyword in dataset["artist_name"]:
+            for keyword in dataset["artist_name"]:
                 st.header("Dados Individuais do Artista Selecionado")
 
                 st.write("Artista pesquisado")
